026ea4452857b6a289ef93184c236477d6442c1d
jacobpew <jacobpew@outlook.com>
2022-08-05 20:41:01 -0400

    update

--- RTSeval.c
+++ RTSeval.c
@@ -3,12 +3,16 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 
+#define SR1_PIN 16
+#define SR2_PIN 17
+#define SRCLK_PIN 18
+
 const uint LED_PIN = 25;
 int counter = 0;
-int inputSequence = 00000000;
+int 0;
 
 int main() {
-	bi_decl(bi_program_description("This is a test binary."));
+	bi_decl(bi_program_description("This is a Shift register binary incremental test."));
 	bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
 
 	stdio_init(LED_PIN);
@@ -16,13 +20,24 @@ int main() {
 
 	while(1) {
 		gpio_put(LED_PIN, 1);
-		for(int byte=0; byte <256, byte++) {
-			//alogrithm code
-			//increment shiftR1 256 bit number
-			//increment shiftR2 256 bit number
-			//clock goes high
-			//shift in first bit for sr1 & sr2 
+		/////algorithm code
+		//store SR instructions to array(s)
+	uint8_t sr1Data[8] = [1,1,1,1,1,1,1,1]
+	uint8_t sr2Data[8] = [1,1,1,1,1,1,1,1]
+
+		for(int byte=0; byte <8, byte++) {
+			
+			//
+	gpio_put(SRCLK_PIN, 1);		//set clock pin high
+	gpio_put(SR1_PIN, sr1Data[byte]);	//load SR1 bit into data pin
+	gpio_put(SR2_PIN, sr2Data[byte]);
+	gpio_put(SRCLK_PIN, 0);
+			//increment SR1 array
+			//repeat for shiftR2 8 bit word PH
 			//clock goes low
+			//
+			//
+			//
 }
 	puts("transistor xx is on");
 }
