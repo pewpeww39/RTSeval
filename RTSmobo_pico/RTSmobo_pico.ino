d55b970d9939f64cc95fed6bf6d1a52b243d66c6
jacobpew <jacobpew@outlook.com>
2023-01-20 22:58:42 -0500

    update

--- RTSmobo_pico/RTSmobo_pico.ino
+++ RTSmobo_pico/RTSmobo_pico.ino
@@ -1,32 +1,26 @@
 #include <SPI.h>
 
 #define debug true
-#define vgHVP   0   // point Vg input voltage to High Voltage Pmos transistors
-#define vgLVP   1   // point Vg input voltage to Low Voltage Pmos transistors
-#define SCL     2   // Shift Register Clock
-#define SDA_A   3   // Vertical Shift Register data pin
-#define SDA_B   4   // Horizontal Shift Register data pin
-#define RESET   5   // Reset shift registers
-#define vddHV   6   // set Vdd input voltage to High Voltage transistors
-#define vgLVN   7   // set VG input voltage to Low Voltage Nmos transistors
-#define vgHVN   8   // set VG input voltaget to High Voltage Nmos Transistors
-#define vddLV   9   // set VDD input voltage to Low Voltage Tansistors
-#define ch1v0   10  // Select which amplifier output is on channel 1  vout0
-#define ch1v4   11  // Select which amplifier output is on channel 1  vout4
-#define ch2v1   12  // Select which amplifier output is on channel 2  vout1
-#define ch2v5   13  // Select which amplifier output is on channel 2  vout5
-#define ch3v2   14  // Select which amplifier output is on channel 3  vout2
-#define ch3v6   15  // Select which amplifier output is on channel 3  vout6
-#define ch4v3   16  // Select which amplifier output is on channel 4  vout3
-#define ch4v7   17  // Select which amplifier output is on channel 4  vout7
-#define Csin    18  // Vout amplifier bypass
-#define smuSel  19  // Select SMU as precision nanoAmp Current Source
-#define naSel   20  // Select on board nA Current Source
-#define ch8v7   21  // Select which amplifier output is on channel 8  vout7
-#define ch7v6   22  // Select which amplifier output is on channel 7  vout6
-#define LED     25  // Pico LED
-#define ch6v5   26  // Select which amplifier output is on channel 6  vout5
-#define ch5v4   27  // Select which amplifier output is on channel 5  vout4
+#define vgHVP           0   // point Vg input voltage to High Voltage Pmos transistors
+#define vgLVP           1   // point Vg input voltage to Low Voltage Pmos transistors
+#define HCLKin          2   // Shift Register Clock
+#define Din             3   // Vertical Shift Register data pin
+#define DHin            4   // Horizontal Shift Register data pin
+#define resetBIN        5   // Reset shift registers
+#define vddHV           6   // set Vdd input voltage to High Voltage transistors
+#define vgLVN           7   // set VG input voltage to Low Voltage Nmos transistors
+#define vgHVN           8   // set VG input voltaget to High Voltage Nmos Transistors
+#define vddLV           9   // set VDD input voltage to Low Voltage Tansistors
+#define ch1_vooff      10  // Select which amplifier output is on channel 1  vout0
+#define ch1vo1          11  // Select which amplifier output is on channel 1  vout4
+#define ch1vo2          12  // Select which amplifier output is on channel 2  vout1
+#define ch1vo3          13  // Select which amplifier output is on channel 2  vout5
+#define ch1vo4          14  // Select which amplifier output is on channel 3  vout2
+#define ch1vo5          15  // Select which amplifier output is on channel 3  vout6
+#define ch1vo6          16  // Select which amplifier output is on channel 4  vout3
+#define ch1vo7          17  // Select which amplifier output is on channel 4  vout7
+#define Csin            18  // Vout amplifier bypass
+#define LED             25  // Pico LED
 
 int command = 0;
 int colSelect = 0;
@@ -44,6 +38,7 @@ void setup()
   }
   definePins();   // set Pinmode for pins
   turnOff();      // digital write pins low
+  flashLED();
 }
 
 void loop()
@@ -55,24 +50,26 @@ void loop()
 
   switch (command) {
     case 0: {   // set all pins low
-        turnOff();
-        flashLED();
+        //turnOff();
         break;
       }
-    case 1: {                               // set channel 1 for amp characterization
-        turnOff();                          // by setting the SR inputs to low
+    case 1: {                               // amp characterization
+        turnOff();
+        digitalWrite(resetBIN, LOW);        // by setting the SR inputs to low
         digitalWrite(Csin, HIGH);           // close amp bypass
-        digitalWrite(ch1v0, HIGH);          // chanel 1 has vout0
-                                            // apply a voltage to vout1BYP jumper
-                                            // adjust gain to provide a 1:1 input/output
+        // apply a voltage to vout1BYP jumper
+        // adjust gain to provide a 1:1 input/output
+        flashLED();
+        command = 0;
+        Serial.println("Ready for OpAmp characterization");
         break;
       }
     case 2: {                               // increment through columns and hold row
         digitalWrite(LED, LOW);
         if (debug == true) {
-            Serial.println("H V");
+          Serial.println("H V");
         }
-        for (int j = 2048; j > 0; j--) {    // for loop for the number of columns
+        for (int j = 256; j > 0; j--) {    // for loop for the number of columns
           if (pow(2, colSelect) == j) {     // check if 2^j = desired column i.e. 0000..0100
             horSR = 1;                      // if it does set SDA_ to high
           } else {
@@ -83,19 +80,18 @@ void loop()
           } else {
             verSR = 0;
           }
-          digitalWrite(RESET, HIGH);        // Flush the SR
+          digitalWrite(resetBIN, HIGH);        // Flush the SR
           waitFor(10);
-          digitalWrite(RESET, LOW);
+          digitalWrite(resetBIN, LOW);
           waitFor(10);
-          digitalWrite(SCL, HIGH);          
+          digitalWrite(HCLKin, HIGH);          // set the SR clock high
           digitalWrite(LED, HIGH);
           waitFor(10);
-          digitalWrite(SDA_A, horSR);
-          digitalWrite(SDA_B, verSR);
+          digitalWrite(Din, horSR);       // set SDA_A pin to horSR value
+          digitalWrite(DHin, verSR);       // set SDA_B pin to verSR value
           waitFor(10);
-          digitalWrite(SCL, LOW);          
+          digitalWrite(HCLKin, LOW);           // set the SR clock Low
           digitalWrite(LED, LOW);
-          waitFor(10);
 
           if (debug == true) {
             Serial.print(horSR );
@@ -118,9 +114,9 @@ void loop()
         digitalWrite(LED, LOW);
         timerB = millis();
         if (debug == true) {
-            Serial.println("H V");
+          Serial.println("H V");
         }
-        for (int j = 2048; j > 0; j--) {    // for loop for the number of columns
+        for (int j = 256; j > 0; j--) {    // for loop for the number of columns
           if (pow(2, colSelect) == j) {     // check if 2^j = desired column i.e. 0000...0100
             horSR = 1;                      // if it does set SDA_ to high
           } else {
@@ -131,21 +127,21 @@ void loop()
           } else {
             verSR = 0;
           }
-          
-          digitalWrite(RESET, HIGH);        // Flush the SR
+
+          digitalWrite(resetBIN, HIGH);        // Flush the SR
           waitFor(10);
-          digitalWrite(RESET, LOW);
+          digitalWrite(resetBIN, LOW);
           waitFor(10);
-          digitalWrite(SCL, HIGH);          // set the SR clock high
+          digitalWrite(HCLKin, HIGH);          // set the SR clock high
           digitalWrite(LED, HIGH);
           waitFor(10);
-          digitalWrite(SDA_A, horSR);       // set SDA_A pin to horSR value
-          digitalWrite(SDA_B, verSR);       // set SDA_B pin to verSR value
+          digitalWrite(Din, horSR);       // set SDA_A pin to horSR value
+          digitalWrite(DHin, verSR);       // set SDA_B pin to verSR value
           waitFor(10);
-          digitalWrite(SCL, LOW);           // set the SR clock Low          
+          digitalWrite(HCLKin, LOW);           // set the SR clock Low
           digitalWrite(LED, LOW);
           waitFor(10);
-          
+
           if (debug == true) {
             Serial.print(horSR );
             Serial.print(' ');
@@ -153,7 +149,7 @@ void loop()
           }
         }
         rowSelect++;
-        if (rowSelect >= 96){
+        if (rowSelect >= 96) {
           rowSelect = 0;
         }
         flashLED();
@@ -168,22 +164,22 @@ void loop()
   }
 }
 
-void checkSerial(){
-  if (Serial.available() > 0){
+void checkSerial() {
+  if (Serial.available() > 0) {
     command = Serial.readString().toInt();
-    if (debug == true){
-    Serial.println(command);
+    if (debug == true) {
+      Serial.println(command);
     }
   }
 }
 
-void waitFor(int msec){
+void waitFor(int msec) {
   timerB = millis();
-  while (int hold = true){
-  if (millis() - timerB >= msec){
-    hold == false;
-    return;
-  } else {}
+  while (int hold = true) {
+    if (millis() - timerB >= msec) {
+      hold == false;
+      return;
+    } else {}
   }
 }
 void horizBinary(int horizDecim)
@@ -205,76 +201,69 @@ void printFullBin(int number)
   Serial.println();
 }
 void flashLED() {
-  if (millis() - timer <= 500) {
-    digitalWrite(LED, HIGH);
+  timer = millis();
+  for (timer; millis()-timer<=1500;) {    
+    if (millis() - timer <= 500) {
+      digitalWrite(LED, HIGH);
+    }
+    if (millis() - timer > 500 & millis() - timer <=1000) {
+      digitalWrite(LED, LOW);
+    }
+    if (millis() - timer > 1000 & millis() - timer <1500) {
+      digitalWrite(LED, HIGH);
+    }
+    if (millis() - timer >= 1500) {
+      digitalWrite(LED, LOW);
+    }
+    if (debug == true){
+    Serial.println(millis()-timer);
   }
-  if (millis() - timer > 500) {
-    digitalWrite(LED, LOW);
-  }
-  if (millis() - timer > 1000) {
-    digitalWrite(LED, HIGH);
-  }
-  if (millis() - timer > 1500) {
-    digitalWrite(LED, LOW);
-    timer = millis();
   }
 }
 void turnOff()
 {
   digitalWrite(vgHVP, LOW);
   digitalWrite(vgLVP, LOW);
-  digitalWrite(SCL, LOW);
-  digitalWrite(SDA_A, LOW);
-  digitalWrite(SDA_B, LOW);
-  digitalWrite(RESET, LOW);
+  digitalWrite(HCLKin, LOW);
+  digitalWrite(Din, LOW);
+  digitalWrite(DHin, LOW);
+  digitalWrite(resetBIN, LOW);
   digitalWrite(vddHV, LOW);
   digitalWrite(vgLVN, LOW);
   digitalWrite(vgHVN, LOW);
   digitalWrite(vddLV, LOW);
-  digitalWrite(ch1v0, LOW);
-  digitalWrite(ch1v4, LOW);
-  digitalWrite(ch2v1, LOW);
-  digitalWrite(ch2v5, LOW);
-  digitalWrite(ch3v2, LOW);
-  digitalWrite(ch3v6, LOW);
-  digitalWrite(ch4v3, LOW);
-  digitalWrite(ch4v7, LOW);
+  digitalWrite(ch1_vooff, LOW);
+  digitalWrite(ch1vo1, LOW);
+  digitalWrite(ch1vo2, LOW);
+  digitalWrite(ch1vo3, LOW);
+  digitalWrite(ch1vo4, LOW);
+  digitalWrite(ch1vo5, LOW);
+  digitalWrite(ch1vo6, LOW);
+  digitalWrite(ch1vo7, LOW);
   digitalWrite(Csin, LOW);
-  digitalWrite(smuSel, LOW);
-  digitalWrite(naSel, LOW);
-  digitalWrite(ch8v7, LOW);
-  digitalWrite(ch7v6, LOW);
   digitalWrite(LED, LOW);
-  digitalWrite(ch6v5, LOW);
-  digitalWrite(ch5v4, LOW);
 }
 
 void definePins()
 {
   pinMode(vgHVP, OUTPUT);
   pinMode(vgLVP, OUTPUT);
-  pinMode(SCL, OUTPUT);
-  pinMode(SDA_A, OUTPUT);
-  pinMode(SDA_B, OUTPUT);
-  pinMode(RESET, OUTPUT);
+  pinMode(HCLKin, OUTPUT);
+  pinMode(Din, OUTPUT);
+  pinMode(DHin, OUTPUT);
+  pinMode(resetBIN, OUTPUT);
   pinMode(vddHV, OUTPUT);
   pinMode(vgLVN, OUTPUT);
   pinMode(vgHVN, OUTPUT);
   pinMode(vddLV, OUTPUT);
-  pinMode(ch1v0, OUTPUT);
-  pinMode(ch1v4, OUTPUT);
-  pinMode(ch2v1, OUTPUT);
-  pinMode(ch2v5, OUTPUT);
-  pinMode(ch3v2, OUTPUT);
-  pinMode(ch3v6, OUTPUT);
-  pinMode(ch4v3, OUTPUT);
-  pinMode(ch4v7, OUTPUT);
+  pinMode(ch1_vooff, OUTPUT);
+  pinMode(ch1vo1, OUTPUT);
+  pinMode(ch1vo2, OUTPUT);
+  pinMode(ch1vo3, OUTPUT);
+  pinMode(ch1vo4, OUTPUT);
+  pinMode(ch1vo5, OUTPUT);
+  pinMode(ch1vo6, OUTPUT);
+  pinMode(ch1vo7, OUTPUT);
   pinMode(Csin, OUTPUT);
-  pinMode(smuSel, OUTPUT);
-  pinMode(naSel, OUTPUT);
-  pinMode(ch8v7, OUTPUT);
-  pinMode(ch7v6, OUTPUT);
   pinMode(LED, OUTPUT);
-  pinMode(ch6v5, OUTPUT);
-  pinMode(ch5v4, OUTPUT);
 }
