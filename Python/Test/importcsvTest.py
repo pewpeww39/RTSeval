1c8c78ab7bbf5f53d7eaf2fec298b72e2fca610e
jacobpew <jacobpew@outlook.com>
2023-02-11 21:09:19 -0500

    update

--- Python/Test/importcsvTest.py
+++ Python/Test/importcsvTest.py
@@ -1,11 +1,21 @@
 import pandas as pd
 
-specData = pd.DataFrame(pd.read_csv('~\miniconda3\envs\\testequ\RTSeval\Files\RTS_Array_Cells.csv', index_col=[0] , header=0), 
-                            columns = ['W', 'L', 'Type'])
+def inport(file, idex, head, col):
+    df = pd.DataFrame(pd.read_csv(file, index_col=[idex] , header=head), 
+                            columns = col)
+    return df
+
+
+# specData = pd.DataFrame(pd.read_csv('~\miniconda3\envs\\testequ\RTSeval\Files\RTS_Array_Cells.csv',
+#                      index_col=[0] , header=0), columns = ['W', 'L', 'Type'])
+
+specData = inport("~\miniconda3\envs\\testequ\RTSeval\Files\RTS_Array_Cells.csv", 0, 0, ['W','L'])
 spec=[]
-print(specData.iloc[1])
+print(specData)
 for v in range(5):
-    spec = list(specData.iloc[v+1])
+    spec = list(specData.iloc[v])
+    if v == 4:
+        print(v)
     print(str(spec))
 
 # C:\Users\jacob\miniconda3\envs\testequ\RTSeval\Files\DOE_RTS_Array_Cells.xlsx
