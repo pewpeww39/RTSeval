e25294492f8aa770cb540c001e1a3778b258037b
jacobpew <jacobpew@outlook.com>
2023-09-01 17:33:49 -0400

    update

--- Python/ampChar_V2.py
+++ Python/ampChar_V2.py
@@ -4,8 +4,7 @@ import time
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
-from keithley2600 import Keithley2600
-# import RTSeval.Python.BKPrecision.lib1785b as bk
+from keithleyDriver import Keithley2600
 from os import system, name
 from datetime import datetime
 
@@ -24,19 +23,21 @@ def write_cmd(x):
     pico.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
 
-data = pd.DataFrame(data=[], index=[], columns=["vIn", "vOut"])           #create dataframe
 
-# int(input('How many steps for current?'))
-# int(input('How many steps for Voltage?'))
+def doe1_ampCharacterization(user, amp):
+    if amp in range(0,4):
+        select = 1                                                            # NMOS Op-Amp
+    elif amp in range(4,8):
+        select = 2                                                            # PMOS Op-Amp
 
-def doe1_ampCharacterization(select):
+    data = pd.DataFrame(data=[], index=[], columns=["vIn", "vOut"])           #create dataframe
+
     smu.smua.OUTPUT_DCVOLTS          # SMU 1 is set to apply voltage
     smu.smub.OUTPUT_DCAMPS           # SUM 2 is set to measure voltage
     smu._write(value='smua.source.output = smua.OUTPUT_ON')
     smu._write(value='smub.source.output = smub.OUTPUT_ON')
-    #bkPS.outputON(True, bkPS)
     write_cmd(f"{select}")  
-    time.sleep(1)
+    time.sleep(0.5)
     commandRX = int(pico.read_until().strip().decode())                             # confirms amp characterization is selected
     if commandRX == 1 or commandRX == 2:
         print('pico selected amp characterization procedure.')
@@ -49,7 +50,7 @@ def doe1_ampCharacterization(select):
         plt.xlabel("Vin")
         plt.ylabel("Vout")
         plt.legend()
-        plt.savefig("C:\\Users\\UTChattsat\\miniconda3\\envs\\testequ\\RTSeval\\Python\\Data\\ampCharacterization\\amp0_VoVsVin.png")
+        plt.savefig("C:\\Users\\" + user + "\\Documents\\LBNL2023\\ampCharacterization\\amp" + str(amp) + "_Vo_vs_Vin.png")
         fig = plt.show()
         plt.pause(3)
         plt.close(fig)
@@ -62,4 +63,7 @@ def doe1_ampCharacterization(select):
 
 NMOS = 1
 PMOS = 2
-doe1_ampCharacterization(NMOS)
+user = str(input("User name: "))
+amp = int(input("Which Op-Amp is being tested? "))
+
+doe1_ampCharacterization(user, amp)
